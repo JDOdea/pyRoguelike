7dd11c9b78b4d609ecc40b947009c1bab3ad8681
JDOdea <jondavidodea@gmail.com>
2025-01-07 13:54:11 -0600

    player leveling system

--- input_handlers.py
+++ input_handlers.py
@@ -29,19 +29,15 @@ MOVE_KEYS = {
     tcod.event.KeySym.s: (0, 1),
     tcod.event.KeySym.a: (-1, 0),
     tcod.event.KeySym.d: (1, 0),
-    tcod.event.KeySym.q: (-1, -1),
-    tcod.event.KeySym.z: (-1, 1),
-    tcod.event.KeySym.e: (1, -1),
-    tcod.event.KeySym.c: (1, 1),
     # Numpad keys.
     tcod.event.KeySym.KP_1: (-1, 1),
     tcod.event.KeySym.KP_2: (0, 1),
     tcod.event.KeySym.KP_3: (1, 1),
     tcod.event.KeySym.KP_4: (-1, 0),
-    tcod.event.KeySym.KP_5: (1, 0),
-    tcod.event.KeySym.KP_6: (-1, -1),
-    tcod.event.KeySym.KP_7: (0, -1),
-    tcod.event.KeySym.KP_8: (1, -1),
+    tcod.event.KeySym.KP_6: (1, 0),
+    tcod.event.KeySym.KP_7: (-1, -1),
+    tcod.event.KeySym.KP_8: (0, -1),
+    tcod.event.KeySym.KP_9: (1, -1),
     # Vi keys.
     tcod.event.KeySym.h: (-1, 0),
     tcod.event.KeySym.j: (0, 1),
@@ -138,6 +134,8 @@ class EventHandler(BaseEventHandler):
             if not self.engine.player.is_alive:
                 # The player was killed sometime during or after the action.
                 return GameOverEventHandler(self.engine)
+            elif self.engine.player.level.requires_level_up:
+                return LevelUpEventHandler(self.engine)
             return MainGameEventHandler(self.engine) # Return to the main handler.
         return self
 
@@ -197,6 +195,119 @@ class AskUserEventHandler(EventHandler):
         """
         return MainGameEventHandler(self.engine)
  
+
+class CharacterScreenEventHandler(AskUserEventHandler):
+    TITLE = "Character Information"
+
+    def on_render(self, console: tcod.console.Console) -> None:
+        super().on_render(console)
+
+        if self.engine.player.x <= 30:
+            x = 40
+        else:
+            x = 0
+
+        y = 0
+
+        width = len(self.TITLE) + 4
+
+        console.draw_frame(
+            x=x,
+            y=y,
+            width=width,
+            height=7,
+            title=self.TITLE,
+            clear=True,
+            fg=(255, 255, 255),
+            bg=(0, 0, 0),
+        )
+
+        console.print(
+            x=x + 1, y=y + 1, string=f"Level: {self.engine.player.level.current_level}"
+        )
+        console.print(
+            x=x + 1, y=y + 2, string=f"XP: {self.engine.player.level.current_xp}"
+        )
+        console.print(
+            x=x + 1, y=y + 3, string=f"XP for next level: {self.engine.player.level.experience_to_next_level}",
+        )
+
+        console.print(
+            x=x + 1, y=y + 4, string=f"Attack: {self.engine.player.fighter.power}"
+        )
+        console.print(
+            x=x + 1, y=y + 5, string=f"Defense: {self.engine.player.fighter.defense}"
+        )
+ 
+
+class LevelUpEventHandler(AskUserEventHandler):
+    TITLE = "Level Up"
+
+    def on_render(self, console: tcod.console.Console) -> None:
+        super().on_render(console)
+
+        if self.engine.player.x <= 30:
+            x = 40
+        else:
+            x = 0
+
+        console.draw_frame(
+            x=x,
+            y=0,
+            width=35,
+            height=8,
+            title=self.TITLE,
+            clear=True,
+            fg=(255, 255, 255),
+            bg=(0, 0, 0),
+        )
+
+        console.print(x=x + 1, y=1, string="Congratulations! You level up!")
+        console.print(x=x + 1, y=2, string="Select an attribute to increase.")
+
+        console.print(
+            x=x + 1,
+            y=4,
+            string=f"a) Constitution (+20 HP, from {self.engine.player.fighter.max_hp})",
+        )
+        console.print(
+            x=x + 1,
+            y=5,
+            string=f"b) Strength (+1 attack, from {self.engine.player.fighter.power})",
+        )
+        console.print(
+            x=x + 1,
+            y=6,
+            string=f"c) Agility (+1 defense, from {self.engine.player.fighter.defense})",
+        )
+
+    def ev_keydown(self, event: tcod.event.KeyDown) -> Optional[ActionOrHandler]:
+        player = self.engine.player
+        key = event.sym
+        index = key - tcod.event.KeySym.a
+
+        if 0 <= index <= 2:
+            if index == 0:
+                player.level.increase_max_hp()
+            elif index == 1:
+                player.level.increase_power()
+            else:
+                player.level.increase_defense()
+        else:
+            self.engine.message_log.add_message("Invalid entry.", color.invalid)
+
+            return None
+        
+        return super().ev_keydown(event)
+    
+    def ev_mousebuttondown(
+            self, event: tcod.event.MouseButtonDown
+    ) -> Optional[ActionOrHandler]:
+        """
+        Don't allow the player to click to exit the menu, like normal.
+        """
+        return None
+
 
 class InventoryEventHandler(AskUserEventHandler):
     """This handler lets the user select an item.
@@ -429,6 +540,9 @@ class MainGameEventHandler(EventHandler):
 
         elif key == tcod.event.KeySym.d:
             return InventoryDropHandler(self.engine)
+        
+        elif key == tcod.event.KeySym.c:
+            return CharacterScreenEventHandler(self.engine)
 
         elif key == tcod.event.KeySym.SLASH:
             return LookHandler(self.engine)   
